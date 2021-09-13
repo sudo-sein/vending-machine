c3fcd372afadca114184048061e74d6a3d308cc6
Blazej Adamczyk <sein.adamczyk@gmail.com>
2021-09-13 11:11:54 +0200

    Working javascript solution

--- js/vending_machine.js
+++ js/vending_machine.js
@@ -3,13 +3,13 @@ const COFFEE_PRICE = 125;
 
 function VendingMachine(dispenser) {
     this.dispenser = dispenser;
-    this.acceptedCoins = [5,20];
-    this.insertedCoins = [0];
+    this.acceptedCoins = [20,5];
+    this.insertedCoins = [];
 }
 
 // Return a boolean to show if the coin was accepted.
-VendingMachine.prototype.insertCoin = function(coinValue) {
-    if (this.acceptedCoins.includes(coinValue)){
+VendingMachine.prototype.insertCoin = function (coinValue) {
+    if (this.acceptedCoins.includes(coinValue)) {
         this.insertedCoins.push(coinValue);
         return true;
     }
@@ -17,9 +17,9 @@ VendingMachine.prototype.insertCoin = function(coinValue) {
 };
 
 // Call serveTea and return the total value of coins returned, or -1 if there are insufficient coins in the machine.
-VendingMachine.prototype.pressTea = function() {
-    const total = this.insertedCoins.reduce((acc, val) => acc+val);
-    if (total > TEA_PRICE) {
+VendingMachine.prototype.pressTea = function () {
+    // const total = this.insertedCoins.reduce((acc, val) => acc + val);
+    if (this.hasCoins(TEA_PRICE)) {
         this.dispenser.serveTea();
         this.consumeCoins(TEA_PRICE);
         return this.pressReturn();
@@ -29,9 +29,10 @@ VendingMachine.prototype.pressTea = function() {
 };
 
 // Call serveCoffee and return the total value of coins returned, or -1 if there are insufficient coins in the machine.
-VendingMachine.prototype.pressCoffee = function() {
-    const total = this.insertedCoins.reduce((acc, val) => acc+val);
-    if (total > COFFEE_PRICE) {
+VendingMachine.prototype.pressCoffee = function () {
+    const total = this.insertedCoins.length > 0 ? this.insertedCoins.reduce((acc, val) => acc + val) : 0;
+
+    if (this.hasCoins(COFFEE_PRICE)) {
         this.dispenser.serveCoffee();
         this.consumeCoins(COFFEE_PRICE);
         return this.pressReturn();
@@ -39,36 +40,60 @@ VendingMachine.prototype.pressCoffee = function() {
     return -1;
 };
 
-VendingMachine.prototype.consumeCoins = function(amount) {
-    var total = 0;
-    this.insertedCoins = this.insertedCoins.map(value => {
-        if (total < amount) {
-            total = total + value;
-            return 0;
+VendingMachine.prototype.hasCoins = function (amount) {
+    const requiredCoins = this.requiredCoins(amount);
+    const contains = [];
+    for (let i = 0; i < requiredCoins.length; i++) {
+        const amountInserted = this.insertedCoins.filter(coin => this.acceptedCoins[i] === coin).length;
+        contains.push(amountInserted >= requiredCoins[i]);
+    }
+    return contains.every(isEnough => isEnough);
+}
+
+VendingMachine.prototype.consumeCoins = function (amount) {
+    const requiredCoins = this.requiredCoins(amount);
+    for (let i = 0; i < requiredCoins.length; i++) {
+        for (let j = 0; j < requiredCoins[i]; j++) {
+            const index = this.insertedCoins.indexOf(this.acceptedCoins[i])
+            this.insertedCoins.splice(index,1)
         }
-        return value;
-    });
+    }
+}
+
+VendingMachine.prototype.requiredCoins = function (amount) {
+    var finalresult = [],
+        totalAmount = amount;
+    for (var i = 0; i < this.acceptedCoins.length; i++) {
+        var currentTypeValue = this.acceptedCoins[i],
+            typeCount = Math.floor(totalAmount / currentTypeValue);
+        finalresult.push(typeCount);
+        totalAmount -= (currentTypeValue * typeCount);
+    }
+    return finalresult;
 };
 
 // Return the total value of coins in the machine.
-VendingMachine.prototype.pressReturn = function() {
-    return this.insertedCoins.reduce((acc, val) => acc+val);
+VendingMachine.prototype.pressReturn = function () {
+    const returnAmount = this.insertedCoins.length > 0 ? this.insertedCoins.reduce((acc, val) => acc + val) : 0;
+    this.insertedCoins = [];
+    return returnAmount;
 };
 
 module.exports = VendingMachine;
 
 
-function DummyDispenser() {}
+function DummyDispenser() {
+}
 
-DummyDispenser.prototype.serveCoffee = function() {
+DummyDispenser.prototype.serveCoffee = function () {
     console.log("Coffee served");
 };
 
-DummyDispenser.prototype.serveTea = function() {
+DummyDispenser.prototype.serveTea = function () {
     console.log("Tea served");
 };
 
 if (require.main === module) {
     var machine = new VendingMachine(new DummyDispenser());
-    console.log(machine.pressCoffee());
+    console.log(machine.requiredCoins(125));
 }
--- js/vending_machine.spec.js
+++ js/vending_machine.spec.js
@@ -1,4 +1,4 @@
-import * as VendingMachine from "./vending_machine";
+const VendingMachine = require("./vending_machine");
 
 
 describe('Vending Machine', function () {
@@ -10,49 +10,83 @@ describe('Vending Machine', function () {
 
 
     it('should accept 5 cents', function () {
-
+        const result = machine.insertCoin(5);
+        expect(result).toBeTruthy();
     });
 
     it('should accept 20 cents', function () {
-
+        const result = machine.insertCoin(20);
+        expect(result).toBeTruthy();
     });
 
-    it('should reject 50 cents', function () {
-
-    });
-
-    it('should dispense coffee when there is enough money', function () {
-
-    });
-
-    it('should dispense tea when there is enough money', function () {
-
+    it('should reject other coins then 5 and 20', function () {
+        const coins = [1,2,10,50];
+        for (let i = 0; i < coins.length; i++) {
+            const result = machine.insertCoin(coins[i]);
+            expect(result).toBeFalsy();
+        }
     });
 
     it('should return -1 for TEA if there is not enough money', function () {
-
+        const result = machine.pressTea();
+        expect(result).toBe(-1);
     });
 
     it('should return -1 for COFFEE if there is not enough money', function () {
-
+        const result = machine.pressCoffee();
+        expect(result).toBe(-1);
     });
 
-    it('should return all the coins if no drink is dispensed', function () {
-
-    });
 
     it('should return change when TEA is dispensed', function () {
 
+        const coffeeCoins = [20,20,20,5,5];
+        coffeeCoins.forEach(coin => machine.insertCoin(coin));
+        const result = machine.pressTea();
+        expect(result).toBe(25);
     });
 
     it('should return change when COFFEE is dispensed', function () {
 
+        const coffeeCoins = [20,20,20,20,20,20,20,5,5];
+        coffeeCoins.forEach(coin => machine.insertCoin(coin));
+        const result = machine.pressCoffee();
+        expect(result).toBe(25);
     });
+
+    it('should dispense tea when there is enough money', function () {
+        console.log = jest.fn();
+        const teaCoins = [20,20,5];
+        teaCoins.forEach(coin => machine.insertCoin(coin));
+        const result = machine.pressTea();
+        expect(result).toBe(0);
+        expect(console.log).toHaveBeenCalledWith('Tea served');
+    });
+
+    it('should dispense coffee when there is enough money', function () {
+        console.log = jest.fn();
+        const coffeeCoins = [20,20,20,20,20,20,5];
+        coffeeCoins.forEach(coin => machine.insertCoin(coin));
+        const result = machine.pressCoffee();
+        expect(result).toBe(0);
+        expect(console.log).toHaveBeenCalledWith('Coffee served');
+    });
+
+
+    it('should return all the coins if no drink is dispensed', function () {
+        const teaCoins = [20,20,5,5,5];
+        teaCoins.forEach(coin => machine.insertCoin(coin));
+        const result = machine.pressReturn();
+        expect(result).toBe(55);
+    });
+
 
     it('should track money correctly', function () {
-
+        const teaCoins = [20,20,5];
+        teaCoins.forEach(coin => machine.insertCoin(coin));
+        const result = machine.pressReturn();
+        expect(result).toBe(45)
     });
-
 
 });
 
@@ -60,9 +94,9 @@ describe('Vending Machine', function () {
 function TestDispenser() {}
 
 TestDispenser.prototype.serveCoffee = function() {
-    return "Coffee served";
+    console.log("Coffee served");
 };
 
 TestDispenser.prototype.serveTea = function() {
-    return "Tea served";
+    console.log("Tea served");
 };
